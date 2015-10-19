f3f8009192aafc7a1f4600440774491adbd3441e
DatKami <studiowolf8@gmail.com>
2015-10-19 13:51:29 -0400

    Started work on joggle ship

--- beaconship/views.py
+++ beaconship/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 from beaconship.models import *
 
 # Create your views here.
+
+"""
+  Something funny happens here.
+  Returns:    A joggled ship
+  Arguments:  id - int - for the pk of the ship
+"""
+def joggle_ship(id):
+  ship = Ships.objects.get(id=id)
+  contenders = BSContender.objects.filter(ship=ship)
+  
+  
